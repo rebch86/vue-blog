c5efad298c3e9a5db0da2ef1a618d690707c3bb8
kyle <kyle@freshcode.me>
2019-01-14 15:04:54 +0900

    [기타] Vuetify 추가 2019.01.14 - CSS 프레임워크 vuetify 추가.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
+    <v-app>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+      <v-content>
+        <v-container fluid>
+          <router-view/>
+        </v-container>
+      </v-content>
+      <v-footer class="pa-3" dark height="42">
+        <v-spacer></v-spacer>
+        <div>&copy; 2019.01 Created By rebch86.(Made of vue / vuetify) </div>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Vuetify
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
 new Vue({
