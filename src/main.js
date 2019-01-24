257d378fb0256f3356dd67f199d235138531ce22
kyle <kyle@freshcode.me>
2019-01-24 15:08:51 +0900

    [기타] 메뉴 목록 추가 2019.01.24
    - 블로그 메뉴 작업.
    - 메뉴 컨텐츠 index 라우팅 및 예시 페이지 작업중.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueCodeHighlight from 'vue-code-highlight'
+
 // Vuetify & material-design-icons
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
+
+Vue.use(VueCodeHighlight)
 
 Vue.config.productionTip = false
 
